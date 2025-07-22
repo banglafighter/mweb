12738ba7cd451c51ca686057c0b03645202766b6
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-22 11:36:29 +0600

    Added Send From directory response

--- mweb/engine/mweb_response.py
+++ mweb/engine/mweb_response.py
@@ -1,21 +1,23 @@
 import asyncio
-from quart import render_template, make_response
+from datetime import datetime
+from quart import render_template, make_response, send_from_directory, Response
 from typing import Any
+from quart.typing import FilePath
 from mw_common import HTTPContentType
 
 
 class MWebResponse:
 
-    @staticmethod
-    async def render_template(template_name_or_list: str | list[str], **context: Any):
+    @classmethod
+    async def render_template(cls, template_name_or_list: str | list[str], **context: Any):
         return await render_template(template_name_or_list, **context)
 
-    @staticmethod
-    def sync_render_template(template_name_or_list: str | list[str], **context: Any):
+    @classmethod
+    def sync_render_template(cls, template_name_or_list: str | list[str], **context: Any):
         return asyncio.run(render_template(template_name_or_list=template_name_or_list, **context))
 
-    @staticmethod
-    async def make_response(content: str | dict | list, headers: dict = None, http_code: int = None):
+    @classmethod
+    async def make_response(cls, content: str | dict | list, headers: dict = None, http_code: int = None):
         if http_code is None:
             http_code = 200
         response = await make_response(content, http_code)
@@ -23,8 +25,8 @@ class MWebResponse:
             response.headers.update(headers)
         return response
 
-    @staticmethod
-    async def json_response(content: dict | list, headers: dict = None, http_code: int = None):
+    @classmethod
+    async def json_response(cls, content: dict | list, headers: dict = None, http_code: int = None):
         if not http_code:
             http_code = 200
 
@@ -33,3 +35,17 @@ class MWebResponse:
 
         headers["Content-Type"] = HTTPContentType.APPLICATION_UNICODE_JSON
         return await MWebResponse.make_response(content=content, headers=headers, http_code=http_code)
+
+    @classmethod
+    async def send_from_directory(cls, directory: FilePath, file_name: str, mimetype: str | None = None, as_attachment: bool = False, attachment_filename: str | None = None, add_etags: bool = True, cache_timeout: int | None = None, conditional: bool = True, last_modified: datetime | None = None) -> Response:
+        return await send_from_directory(
+            directory=directory,
+            file_name=file_name,
+            mimetype=mimetype,
+            as_attachment=as_attachment,
+            attachment_filename=attachment_filename,
+            add_etags=add_etags,
+            cache_timeout=cache_timeout,
+            conditional=conditional,
+            last_modified=last_modified
+        )
